66f8d0a57f486487c9e049b3accf18ada7498bb7
bgrich <brian.g.richards@gmail.com>
2016-08-15 11:11:45 -0400

    Fix extra documentation.

--- R/lorentzian_fit.R
+++ R/lorentzian_fit.R
@@ -24,7 +24,7 @@ fitCauchy <- function(x, y, center, hwhm, height, yoff){
   optim(c(center, hwhm, height, yoff), f)
 }
 
-#' @describeIn fitG A Lorentzian/Cauchy fit without the y-offset.
+#' @describeIn fitCauchy A Lorentzian/Cauchy fit without the y-offset.
 fitCauchy2 <- function(x, y, center, hwhm, height){
 
   f = function(p){
