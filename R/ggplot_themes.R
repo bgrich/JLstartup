9e815a487912e6b2564c039ef9ac5fd0ba320bdb
bgrich <brian.g.richards@gmail.com>
2016-08-24 13:50:49 -0400

    Add ggplot theme file.

---
+++ R/ggplot_themes.R
@@ -0,0 +1,4 @@
+report_theme <- theme_bw() +
+  theme(panel.border = element_rect(color = "black", fill = NA),
+        panel.grid.major = element_line(color = "black", linetype = "dotted"),
+        panel.grid.minor = element_line(color = "black", linetype = "dotted"))
