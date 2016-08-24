22b6aa2ef8621dcc6fbc3a24f0eb87913533b95e
bgrich <brian.g.richards@gmail.com>
2016-08-24 14:39:35 -0400

    Add publication theme.

--- R/ggplot_themes.R
+++ R/ggplot_themes.R
@@ -17,4 +17,15 @@ report_theme <- ggplot2::theme_bw() +
                                                           linetype = "dotted"))
 
 #' @rdname report_theme
-publication_theme
+publication_theme <- theme_bw() +
+  theme(text = element_text(family = "Times"),
+        axis.title = element_text(size = 13),
+        axis.text = element_text(size = 8),
+        plot.title = element_text(size = 8)) +
+  theme(legend.position = "none",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(color = "black"),
+        axis.ticks.length = unit(-0.15, "cm"),
+        axis.text.x = element_text(margin = margin(t = 0.25, unit = "cm")),
+        axis.text.y = element_text(margin = margin(r = 0.25, unit = "cm")))
