a5ca6f23ff961363f8cde24c6139d48fdc27d2f8
bgrich <brian.g.richards@gmail.com>
2016-08-24 14:00:48 -0400

    Reflow lines and add documentation.

--- R/ggplot_themes.R
+++ R/ggplot_themes.R
@@ -1,4 +1,14 @@
-report_theme <- theme_bw() +
-  theme(panel.border = element_rect(color = "black", fill = NA),
-        panel.grid.major = element_line(color = "black", linetype = "dotted"),
-        panel.grid.minor = element_line(color = "black", linetype = "dotted"))
+#' Report Theme
+#'
+#' Theme
+#'
+#' Theme Descript
+#'
+#' @export
+report_theme <- ggplot2::theme_bw() +
+  ggplot2::theme(panel.border = ggplot2::element_rect(color = "black",
+                                                      fill = NA),
+                 panel.grid.major = ggplot2::element_line(color = "black",
+                                                          linetype = "dotted"),
+                 panel.grid.minor = ggplot2::element_line(color = "black",
+                                                          linetype = "dotted"))
